22cdd48bfe5c6860d372ffcb0d1368489f17ab3f
Raboliot De Paasu <Raboliot De Paasu>
2017-03-21 09:18:49 +0100

    Forgot to close file after reading it

--- module/loading.py
+++ module/loading.py
@@ -5,7 +5,9 @@ def loadJsonFile(path):
     # Opening json file given by the path
     # Returning a python dictionnary
     f = open(path,'r');
-    return json.load(f);
+    fileLoaded = json.load(f);
+    f.close();
+    return fileLoaded;
 
 def saveOptions(path, options):
     # With à given path and a Python dictionnary
