2a7c631acb6f22cabb8ba05b4448cfc58c74dfa9
raboliotthegreat <raboliotthegreat@mailoo.org>
2017-03-21 01:40:13 +0100

    test of loading module ine main

--- MirrorRush.py
+++ MirrorRush.py
@@ -1,5 +1,5 @@
-# RaboSchmup
-# Made by Raboliot
+# MirrorRush
+# Made by Salle14 Lab
 
  #-*- coding:Utf-8 -*-
 
@@ -12,14 +12,19 @@ import os;
 #On definit le path
 sys.path.append(os.path.realpath('.\module'));
 sys.path.append(os.path.realpath('.\class'));
-#os.path.join(os.path.dirname(__file__), '.\module');
 
-print(sys.path);
-
+#Importation des modules customs
 import util;
+import loading;
+
+#Global variable
+#Maybe set a global dict for all global variable
+optionsPath = ".\data\options.json";
 
 #init pygame
 pygame.init();
+options = loading.loadJsonFile(optionsPath);
+#result = loading.saveOptions(optionsPath,options);
 
 #Creating the window
 widowResolution = pygame.display.Info();
@@ -30,6 +35,3 @@ window  = pygame.display.set_mode((widowResolution.current_w,widowResolution.cur
 img = util.load_image('.\data\img\BG.jpg');
 window.blit(img,(0,0));
 pygame.display.flip();
-
-while 1:
-    print("coucou");
