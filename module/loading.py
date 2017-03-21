a4ff2ebac0f1036c83097d069176c0233b5561f0
raboliotthegreat <raboliotthegreat@mailoo.org>
2017-03-21 01:33:49 +0100

    add file loading system

---
+++ module/loading.py
@@ -0,0 +1,15 @@
+import json;
+from io import StringIO;
+
+def loadJsonFile(path):
+    # Opening json file given by the path
+    # Returning a python dictionnary
+    f = open(fileToOpen,'r');
+    return json.load(f);
+
+def saveOptions(path, options):
+    # With à given path and a Python dictionnary
+    # Saving it in a Json file
+    f = open(path,'w');
+    f.write(json.JSONEncoder().encode(options));
+    f.close();
