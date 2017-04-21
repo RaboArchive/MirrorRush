6bacd71a4d0f5281ef1fd53baa38e1dc734778e0
raboliotthegreat <raboliotthegreat@mailoo.org>
2017-04-21 20:54:42 +0200

    dont remember

--- classes/menu.py
+++ classes/menu.py
@@ -5,29 +5,29 @@ class menu():
     #constructeur
     def __init__(self, options, path):
         #Initialisations du paths du JSON et sauvegarde des options pour modification futur
-        self._path = path;
-        self._options = options;
-        self._components = [];
-        self._loadComponents(path);
+        self.__path = path;
+        self.__options = options;
+        self.__components = [];
+        self.__loadComponents(path);
 
     #Class methods TODO
-    def loadComponents(path):
+    def __loadComponents(path):
         components = util.loadJsonFile(self.getPath()):
         print(components);
     #def reloadComponents():
-    #def onClickAction()
-    #def modifyOptions
+    #def __onClickAction()
+    #def __modifyOptions
 
     #Getters
     def getPath(self):
-        return self._path;
+        return self.__path;
     def getOptions(self):
-        return self._options;
+        return self.__options;
     def getComponents(self):
-        return self._components;
+        return self.__components;
     def getComponent(self, index):
-        return self._components[i];
+        return self.__components[i];
     #Setters
     def setPath(self, newPath):
         if(newPath != ""):
-            self._path = newPath;
+            self.__path = newPath;
